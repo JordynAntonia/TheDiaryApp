42390ab9536f71da3954dc74e52a99db8f293c77
Jordyn <jordyn@example.com>
2022-02-28 03:09:24 -0500

    running into the iterm 2 and midldeware issue again

--- my-app/src/components/AboutMe.js
+++ my-app/src/components/AboutMe.js
@@ -2,7 +2,8 @@ import React from "react";
 
 const AboutMe = () => {
     return (
-        <div className="card" style={{ boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", borderRadius: "50px"}}>
+        <div className="card" style={{ boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)", borderRadius: "50px", display: "block" , 
+        width: "40px", height: "40px"}}>
             <div className="container" style={{textAlign: "center", overflowWrap: "auto"}}>
                 <h3> Hi, my name is Jordyn! </h3>
                 <p>I'm a Full Stack Software Developer graduate with a passion for creating amazing technology experiences
